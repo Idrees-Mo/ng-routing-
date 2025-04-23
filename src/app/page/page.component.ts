64c46918081fae53d2be0e84dba25146ce59fbe0
Muhammad Idrees <midrees@summitinnovations.com>
2025-04-23 19:39:37 +1000

    wip: add components

---
+++ src/app/page/page.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './page.component.html',
+  styleUrl: './page.component.css',
+})
+export class PageComponent {
+  pageId = input.required<string>(); // This would work if the routes are setup with component input binding
+  limit = input.required<number>(); // in the url, e.g. /pages/1?limit=10
+}
